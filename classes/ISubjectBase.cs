f4978164cebca21081e08c6d9c72a03a8da8c635
ElijahKR <2063390754@qq.com>
2017-02-08 22:25:32 +0800

    Add files via upload

---
+++ classes/ISubjectBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLoader
+{
+    public interface ISubjectBase
+    {
+        void Show(string txt);
+        int Add(int a, int b);
+    }
+}
